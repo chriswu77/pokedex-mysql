9db180599a0eaf6f594621381844253130b05ca4
Chris Wu <cwu0569@gmail.com>
2021-05-31 01:50:28 -0700

    step 1 complete

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
 // TODO: Create an express server
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`server listening on port ${port}`);
+  }
+});
