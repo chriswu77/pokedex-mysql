b48601a8777b6e25d338ca3b5ac48287e2f7ca91
Chris Wu <cwu0569@gmail.com>
2021-05-31 02:00:14 -0700

    step 2 complete

--- server/db/index.js
+++ server/db/index.js
@@ -1,1 +1,21 @@
 // TODO: Establish connection to mysql database
+const mysql = require('mysql');
+
+const dbName = 'pokedex';
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: dbName
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log('couldn\'t connect to database');
+  } else {
+    console.log(`connected to database: ${dbName}`);
+  }
+});
+
+module.exports = db;
